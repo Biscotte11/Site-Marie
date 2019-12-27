3e4a33358dd1ad47a3411b040489c04920fa5865
Biscotte11 <s.volt@hotmail.fr>
2019-12-27 17:00:38 +0100

    firt commit

---
+++ application/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Database configuration settings used by PDO.
+ */
+
+$config['dsn']      = 'mysql:host=localhost;dbname=Musique';
+$config['password'] = 'mysql';
+$config['user']     = 'root';
