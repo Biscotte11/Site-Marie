650f3fe3cd6ac00f0410601ad07c2e8eab8c74c5
Biscotte11 <s.volt@hotmail.fr>
2019-12-31 16:04:32 +0100

    fix conflicts

--- application/www/js/main.js
+++ application/www/js/main.js
@@ -1,23 +1,21 @@
 'use strict';
 // console.log('coucou')
 
-<<<<<<< HEAD
-=======
+
 $('#user--connect').on('mouseover', function() {
   // console.log('test')
 	$('.under-nav-list').removeClass('hide');
 });
->>>>>>> a0b29d797099ba12b2df94e96bb48ed0999445e3
 
 $('#liens').on('mouseover', function() {
 	$('.nav--list').removeClass('hide');
 });
 
-<<<<<<< HEAD
-$('.nav--list').on('mouseout', function() {
-	$('.nav--list').addClass('hide');
-});
-=======
+
+// $('.nav--list').on('mouseout', function() {
+// 	$('.nav--list').addClass('hide');
+// });
+
 $('.under-nav-list').on('mouseout', function() {
 	$('.under-nav-list').addClass('hide');
 });
@@ -30,4 +28,3 @@ $('.under-nav-list').on('mouseout', function() {
 //     // on supprime la classe lorsqu'on sort du rollHover
 //     $('.title--M').removeClass('opacityClass');
 // });
->>>>>>> a0b29d797099ba12b2df94e96bb48ed0999445e3
